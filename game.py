533d098dbfb221660dcfe11c6be0ad0c0619f9ea
William Mead <will@Williams-iMac.local>
2020-05-23 18:25:05 +0100

    tidying repo

--- game.py
+++ game.py
@@ -1,36 +1,46 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
 import random
-
-ON = 255
-OFF = 0
-vals = [ON, OFF]
+import copy
 
 
 def get_random_grid(size):
-    return np.random.choice(vals, size*size, p=[0.2, 0.8]).reshape(size, size)
+    grid = []
+    for row in range(size):
+        grid.append([])
+        for column in range(size):
+            val = 1 if random.randint(1, 5) == 5 else 0
+            grid[row].append(val)
+    print_grid(grid)
+    return(grid)
 
 
-def update(frame_num, img, grid, size):
-    new_grid = grid.copy()
+def print_grid(grid):
+    for row in grid:
+        print(f"{row}\n")
 
-    for i in range(size):
-        for j in range(size):
-            neighbours = int((grid[i, (j-1) % size] + grid[i, (j+1) % size] +
-                              grid[(i-1) % size, j] + grid[(i+1) % size, j] +
-                              grid[(i-1) % size, (j-1) % size] + grid[(i-1) % size, (j+1) % size] +
-                              grid[(i+1) % size, (j-1) % size] + grid[(i+1) % size, (j+1) % size])/255)
 
-            if grid[i, j] == ON:
-                if (neighbours < 2) or (neighbours > 3):
-                    new_grid[i, j] = OFF
-            else:
-                if neighbours == 3:
-                    new_grid[i, j] = ON
+def get_live_neighbors(row, col, size, grid):
+    neighbours = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if not (i == 0 and j == 0):
+                neighbours += grid[((row + i) % size)][((col + j) % size)]
+    return neighbours
 
-    img.set_data(new_grid)
-    grid[:] = new_grid[:]
+
+def update(frame, img, grid, size):
+    grid_copy = copy.deepcopy(grid)
+    for row in range(size):
+        for column in range(size):
+            neighbours = get_live_neighbors(row, column, size, grid_copy)
+
+            if grid_copy[row][column] == 1 and neighbours not in [2, 3]:
+                grid[row][column] = 0
+            elif grid_copy[row][column] == 0 and neighbours == 3:
+                grid[row][column] = 1
+
+    img.set_data(grid)
     return img,
 
 
@@ -40,9 +50,10 @@ grid = get_random_grid(size)
 
 fig, ax = plt.subplots()
 img = ax.imshow(grid, interpolation='nearest')
-plt.axis('off')
-ani = animation.FuncAnimation(fig, update, fargs=(img, grid, size),
-                              frames=10,
-                              interval=interval,
-                              save_count=50)
+ani = FuncAnimation(fig,
+                    update,
+                    fargs=(img, grid, size),
+                    frames=10,
+                    interval=50,
+                    save_count=50)
 plt.show()
